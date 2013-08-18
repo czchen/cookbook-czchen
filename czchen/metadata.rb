00886c9169634cc1a78e82bce1e5022aecc56765
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-08-18 10:35:58 +0800

    Add Berksfile and metadata.rb

---
+++ czchen/metadata.rb
@@ -0,0 +1,6 @@
+name             'czchen'
+maintainer       'ChangZhuo Chen'
+maintainer_email 'czchen@gmail.com'
+license          'MIT'
+description      'Setup czchen\'s environment'
+version          '0.0.1'
