b9dbb20a98cedbcdc3beb8788d0d63a2d8968364
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-11-22 20:41:50 +0800

    Add nodejs, python, ruby packages

--- czchen/recipes/user.rb
+++ czchen/recipes/user.rb
@@ -1,9 +1,27 @@
 require_package = %w(
     git
+    npm
+    python-pip
     python3
+    ruby
     sudo
     vim-gnome
     zsh
+)
+
+pip_package = %w(
+    flake8
+    pip
+    virtualenvwrapper
+)
+
+gem_package = %w(
+)
+
+npm_package = %w(
+    LiveScript
+    n
+    npm
 )
 
 require_package.each do |package_name|
@@ -51,4 +69,45 @@ execute 'setup vim' do
     cwd node[:user][:home]
     command 'vim +BundleInstall +qall'
     subscribes :run, resources(:execute => 'deploy dotfiles')
+end
+
+execute 'setup python' do
+    user node[:user][:user]
+    group node[:user][:group]
+    cwd node[:user][:home]
+
+    pip_package.each do |package|
+        command "pip install #{package}"
+    end
+    not_if { pip_package.empty? }
+
+    subscribes :run, resources(:execute => 'deploy dotfiles')
+    # FIXME: remove global python-pip in favor of local pip
+end
+
+execute 'setup ruby' do
+    user node[:user][:user]
+    group node[:user][:group]
+    cwd node[:user][:home]
+
+    gem_package.each do |package|
+        command "gem install #{package}"
+    end
+    not_if { gem_package.empty? }
+
+    subscribes :run, resources(:execute => 'deploy dotfiles')
+end
+
+execute 'setup nodejs' do
+    user node[:user][:user]
+    group node[:user][:group]
+    cwd node[:user][:home]
+
+    npm_package.each do |package|
+        command "npm install -g #{package}"
+    end
+    not_if { npm_package.empty? }
+
+    subscribes :run, resources(:execute => 'deploy dotfiles')
+    # FIXME: remove global npm in favor of local npm
 end
