79aca1774417e7d18716dacca97a873449572679
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-12-07 00:12:30 +0800

    Move package lists to attributes

--- czchen/attributes/default.rb
+++ czchen/attributes/default.rb
@@ -1,7 +1,54 @@
-default[:user][:user] = 'czchen'
-default[:user][:group] = default[:user][:user]
-default[:user][:home] = "/home/#{default[:user][:user]}"
-default[:user][:shell] = '/bin/zsh'
-default[:user][:dotfiles] = "#{default[:user][:home]}/.dotfiles"
-default[:user][:repository][:https] = 'https://github.com/czchen/dotfiles.git'
-default[:user][:repository][:ssh] = 'git@github.com:czchen/dotfiles.git'
+default[:user] = {
+    :user  => 'czchen',
+    :group => 'czchen',
+}
+
+default[:package][:system] = %w(
+    build-essential
+    git
+    manpages
+    manpages-dev
+    manpages-posix
+    manpages-posix-dev
+    npm
+    python-pip
+    ruby
+    sudo
+    vcsh
+    vim-gnome
+    zsh
+)
+
+default[:package][:vcsh] = {
+    :bzr             => 'https://github.com/czchen/bzr.vcsh',
+    :debian          => 'https://github.com/czchen/debian.vcsh',
+    :gdb             => 'https://github.com/czchen/gdb.vcsh',
+    :gem             => 'https://github.com/czchen/gem.vcsh',
+    :git             => 'https://github.com/czchen/git.vcsh',
+    :gpg             => 'https://github.com/czchen/gpg.vcsh',
+    :hg              => 'https://github.com/czchen/hg.vcsh',
+    :mutt            => 'https://github.com/czchen/mutt.vcsh',
+    :npm             => 'https://github.com/czchen/npm.vcsh',
+    :python          => 'https://github.com/czchen/python.vcsh',
+    :rime            => 'https://github.com/czchen/rime.vcsh',
+    'sublime-text-3' => 'https://github.com/czchen/sublime-text-3.vcsh',
+    :tmux            => 'https://github.com/czchen/tmux.vcsh',
+    :vim             => 'https://github.com/czchen/vim.vcsh',
+    :zsh             => 'https://github.com/czchen/zsh.vcsh',
+}
+
+default[:package][:pip] = %w(
+    flake8
+    pip
+    virtualenvwrapper
+)
+
+default[:package][:gem] = %w(
+    tmuxinator
+)
+
+default[:package][:npm] = %w(
+    LiveScript
+    n
+    npm
+)
--- czchen/recipes/environment.rb
+++ czchen/recipes/environment.rb
@@ -1,55 +1,5 @@
-package = %w(
-    build-essential
-    git
-    manpages
-    manpages-dev
-    manpages-posix
-    manpages-posix-dev
-    npm
-    python-pip
-    ruby
-    sudo
-    vcsh
-    vim-gnome
-    zsh
-)
-
-pip_package = %w(
-    flake8
-    pip
-    virtualenvwrapper
-)
-
-gem_package = %w(
-    tmuxinator
-)
-
-npm_package = %w(
-    LiveScript
-    n
-    npm
-)
-
-vcsh_repo = {
-    :bzr             => 'https://github.com/czchen/bzr.vcsh',
-    :debian          => 'https://github.com/czchen/debian.vcsh',
-    :gdb             => 'https://github.com/czchen/gdb.vcsh',
-    :gem             => 'https://github.com/czchen/gem.vcsh',
-    :git             => 'https://github.com/czchen/git.vcsh',
-    :gpg             => 'https://github.com/czchen/gpg.vcsh',
-    :hg              => 'https://github.com/czchen/hg.vcsh',
-    :mutt            => 'https://github.com/czchen/mutt.vcsh',
-    :npm             => 'https://github.com/czchen/npm.vcsh',
-    :python          => 'https://github.com/czchen/python.vcsh',
-    :rime            => 'https://github.com/czchen/rime.vcsh',
-    'sublime-text-3' => 'https://github.com/czchen/sublime-text-3.vcsh',
-    :tmux            => 'https://github.com/czchen/tmux.vcsh',
-    :vim             => 'https://github.com/czchen/vim.vcsh',
-    :zsh             => 'https://github.com/czchen/zsh.vcsh',
-}
-
-package.each do |package_name|
-    package package_name do
+node[:package][:system].each do |item|
+    package item do
         action :install
     end
 end
@@ -60,7 +10,7 @@ group 'sudo' do
     append true
 end
 
-vcsh_repo.each do |key, value|
+node[:package][:vcsh].each do |key, value|
     execute "deploy #{key}.vcsh" do
         user node[:user][:user]
         group node[:user][:group]
@@ -73,45 +23,34 @@ execute 'setup vim' do
     user node[:user][:user]
     group node[:user][:group]
     cwd node[:user][:home]
-    command 'vim +BundleInstall +qall ; true'
+    command 'vim +BundleInstall +qall'
+    ignore_failure true
     subscribes :run, resources(:execute => 'deploy vim.vcsh')
 end
 
-execute 'setup python' do
-    user node[:user][:user]
-    group node[:user][:group]
-    cwd node[:user][:home]
-
-    pip_package.each do |package|
-        command "pip install #{package}"
+node[:package][:pip].each do |item|
+    execute item do
+        user node[:user][:user]
+        group node[:user][:group]
+        command "echo pip install --upgrade --force-reinstall #{item}"
+        subscribes :run, resources(:execute => 'deploy python.vcsh')
     end
-    not_if { pip_package.empty? }
-
-    subscribes :run, resources(:execute => 'deploy python.vcsh')
 end
 
-execute 'setup ruby' do
-    user node[:user][:user]
-    group node[:user][:group]
-    cwd node[:user][:home]
-
-    gem_package.each do |package|
-        command "gem install #{package}"
+node[:package][:gem].each do |item|
+    execute item do
+        user node[:user][:user]
+        group node[:user][:group]
+        command "gem install #{item}"
+        subscribes :run, resources(:execute => 'deploy gem.vcsh')
     end
-    not_if { gem_package.empty? }
-
-    subscribes :run, resources(:execute => 'deploy gem.vcsh')
 end
 
-execute 'setup nodejs' do
-    user node[:user][:user]
-    group node[:user][:group]
-    cwd node[:user][:home]
-
-    npm_package.each do |package|
-        command "npm install -g #{package}"
+node[:package][:npm].each do |item|
+    execute item do
+        user node[:user][:user]
+        group node[:user][:group]
+        command "npm install -g #{item}"
+        subscribes :run, resources(:execute => 'deploy npm.vcsh')
     end
-    not_if { npm_package.empty? }
-
-    subscribes :run, resources(:execute => 'deploy npm.vcsh')
 end
