9dd6ff9c712c0aec6cdb61705fad654b872cd9b5
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-12-07 14:41:51 +0800

    Fix node prefix

--- czchen/recipes/environment.rb
+++ czchen/recipes/environment.rb
@@ -66,7 +66,7 @@ node[:package][:npm].each do |item|
     execute "npm #{item}" do
         user node[:user][:user]
         group node[:user][:group]
-        command "npm install -g --folder #{node[:package][:config][:npm][:prefix]} #{item}"
+        command "npm_config_prefix=#{node[:package][:config][:npm][:prefix]} npm install -g #{item}"
         subscribes :run, resources(:execute => 'deploy npm.vcsh')
     end
 end
